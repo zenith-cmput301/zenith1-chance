fd600a1145e3f964ca2f1d3eedd27a74a6b48b61
ChauAnh-Cyber-Security <90129332+ChauAnh-Cyber-Security@users.noreply.github.com>
2025-11-02 17:08:10 -0700

    Adjust page design

--- app/src/main/java/com/example/zenithchance/Event.java
+++ app/src/main/java/com/example/zenithchance/Event.java
@@ -1,12 +1,14 @@
 package com.example.zenithchance;
 
+import java.util.Date;
+
 public class Event {
-    public String date;
+    public Date date;
     public String name;
     public String location;
     public String status;
 
-    public Event(String date, String name, String location, String status) {
+    public Event(Date date, String name, String location, String status) {
         this.date = date;
         this.name = name;
         this.location = location;
