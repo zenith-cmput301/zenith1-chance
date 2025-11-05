5b6b89ff48019a2987a52e330fa598b3768409df
Aayush03022003 <aayushparashar2007@gmail.com>
2025-11-04 17:00:59 -0700

    Commit aayush

--- app/src/main/java/com/example/zenithchance/MainActivity.java
+++ app/src/main/java/com/example/zenithchance/MainActivity.java
@@ -1,24 +1,36 @@
-package com.example.zenithchance;
+package com.example.organizer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnPeople;
+    private Button btnEdit;
+    private Button btnMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Initialize buttons
+        btnPeople = findViewById(R.id.btnPeople);
+        btnEdit = findViewById(R.id.btnEdit);
+        btnMap = findViewById(R.id.btnMap);
+
+        btnPeople.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PeopleActivity.class);
+                startActivity(intent);
+            }
         });
+
     }
 }
+
---
+++ app/src/main/java/com/example/zenithchance/PeopleActivity.java
@@ -0,0 +1,6 @@
+package com.example.organizer;
+
+public class PeopleActivity{
+
+
+}
