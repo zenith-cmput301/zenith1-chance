4f58ef41e314049a089bc38a304ef049d75bfeee
Sabrina <sabrinaghadieh@gmail.com>
2025-11-05 20:13:28 -0700

    update admin-pages

---
+++ app/src/main/java/com/example/zenithchance/activities/AdminBrowseProfilesActivity.java
@@ -0,0 +1,8 @@
+package com.example.zenithchance.activities;
+
+public class AdminBrowseProfilesActivity {
+
+
+
+
+}
---
+++ app/src/main/java/com/example/zenithchance/fragments/AdminBrowseProfilesFragment.java
@@ -0,0 +1,10 @@
+package com.example.zenithchance.fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class AdminBrowseProfilesFragment extends Fragment {
+
+
+
+
+}
