e77aa05637b23dd5867a6698cc2bf587f4569433
Kira <ksamaze973@gmail.com>
2025-11-05 20:29:58 -0700

    Admin Nav Added

--- app/src/main/java/com/example/zenithchance/navigation/OrganizerNavigationHelper.java
+++ app/src/main/java/com/example/zenithchance/navigation/OrganizerNavigationHelper.java
@@ -1,4 +1,4 @@
-package com.example.zenithchance;
+package com.example.zenithchance.navigation;
 
 // TODO : To be implemented. See EntrantMainActivity, EntrantNavigationHelper, and entrant_nav_menu.xml for example.-->
 public class OrganizerNavigationHelper {
