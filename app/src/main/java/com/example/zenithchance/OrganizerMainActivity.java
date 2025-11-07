81eca8d21124766d2c2880a17e8a183cff9b2759
emegriffioen <emegriffioen@gmail.com>
2025-11-06 17:06:25 -0700

    Event creation added

--- app/src/main/java/com/example/zenithchance/OrganizerMainActivity.java
+++ app/src/main/java/com/example/zenithchance/OrganizerMainActivity.java
@@ -6,7 +6,10 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.zenithchance.fragments.OrganizerCreateEventFragment;
+import com.example.zenithchance.managers.UserManager;
+import com.example.zenithchance.models.Entrant;
 import com.example.zenithchance.models.Event;
+import com.example.zenithchance.models.Organizer;
 import com.example.zenithchance.navigation.EntrantNavigationHelper;
 import com.example.zenithchance.navigation.OrganizerNavigationHelper;
 
@@ -22,15 +25,21 @@ import java.util.Date;
  */
 public class OrganizerMainActivity extends AppCompatActivity {
 
+    Organizer currentOrganizer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organizer_main);
 
+        // get organizer info
+        currentOrganizer = (Organizer) UserManager.getInstance().getCurrentUser();
+
         // Set up the bottom navigation using the helper
         OrganizerNavigationHelper.setupBottomNav(this);
     }
+
+    public Organizer getOrganizer() { return currentOrganizer; }
 }
 
 //
