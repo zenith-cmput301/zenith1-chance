1765ea8de196f3f0ec7048a4741e0ab9594ca4ec
ChauAnh-Cyber-Security <90129332+ChauAnh-Cyber-Security@users.noreply.github.com>
2025-11-06 22:16:43 -0700

    stash changes

--- app/src/main/java/com/example/zenithchance/models/Organizer.java
+++ app/src/main/java/com/example/zenithchance/models/Organizer.java
@@ -2,13 +2,23 @@ package com.example.zenithchance.models;
 
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  * This class represents Organizer users.
  */
 public class Organizer extends User implements Serializable {
+    private ArrayList<String> orgEvents = new ArrayList<String>(); // Firestore document ids
 
     public Organizer() {
         setType("organizer");
+    }
+
+    public ArrayList<String> getOrgEvents() {
+        return orgEvents;
+    }
+
+    public void setOrgEvents(ArrayList<String> orgEvents) {
+        this.orgEvents = orgEvents;
     }
 }
